23586a6b6eeb991f32bab7d1af32e5057c31348d
Ponomarenko Oleh <albka1986@gmail.com>
2017-02-15 09:41:01 +0200

    updated;

--- app/src/main/java/com/kaa_solutions/eazyback/db/DBHelper.java
+++ app/src/main/java/com/kaa_solutions/eazyback/db/DBHelper.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHelper extends SQLiteOpenHelper {
+final class DBHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     private static final String DATABASE_NAME = "eazyback_db";
 
-    public DBHelper(Context context) {
+    DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
