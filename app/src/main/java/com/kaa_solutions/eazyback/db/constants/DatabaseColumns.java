a2a807bcd77e885b2586eae9dd4efb7a4bddc638
Ponomarenko Oleh <albka1986@gmail.com>
2017-02-17 18:08:21 +0200

    added COLUMN_TIME_LAST_DELAYED_CALL;

--- app/src/main/java/com/kaa_solutions/eazyback/db/constants/DatabaseColumns.java
+++ app/src/main/java/com/kaa_solutions/eazyback/db/constants/DatabaseColumns.java
@@ -8,11 +8,11 @@ public class DatabaseColumns {
 
     }
 
-
     public static class DelayContacts implements BaseColumns {
         public static final String TABLE_NAME = "delay_contacts";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_TIME_LAST_DELAYED_CALL = "time_last_delayed_call";
     }
 
 }
