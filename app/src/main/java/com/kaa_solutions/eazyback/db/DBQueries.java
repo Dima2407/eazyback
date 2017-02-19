896fc07c26df29ce3d4f97146208e3cd3c1c07e2
Ponomarenko <albka1986@gmail.com>
2017-02-19 12:47:12 +0200

    deleted AUTOINCREMENT, added type of column datetime;

--- app/src/main/java/com/kaa_solutions/eazyback/db/DBQueries.java
+++ app/src/main/java/com/kaa_solutions/eazyback/db/DBQueries.java
@@ -8,7 +8,7 @@ public class DBQueries {
     private static final String INTEGER_TYPE = " INTEGER";
     private static final String REAL_TYPE = " REAL";
     private static final String TEXT_TYPE = " TEXT";
-    private static final String DATETIME = "DATETIME";
+    private static final String DATETIME = " DATETIME";
     private static final String BOOLEAN_TYPE = INTEGER_TYPE + BOOLEAN_DEFAULT;
     private static final String COMA_SEP = " , ";
     private static final String PRIMARY_KEY = " PRIMARY KEY";
@@ -20,10 +20,10 @@ public class DBQueries {
     private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS ";
     public static final String CREATE_DELAY_CONTACTS =
             CREATE_TABLE + DatabaseColumns.DelayContacts.TABLE_NAME + " (" +
-                    DatabaseColumns.DelayContacts._ID + INTEGER_TYPE + PRIMARY_KEY + AUTOINCREMENT + COMA_SEP +
+                    DatabaseColumns.DelayContacts._ID + INTEGER_TYPE + PRIMARY_KEY + COMA_SEP +
                     DatabaseColumns.DelayContacts.COLUMN_NAME + TEXT_TYPE + COMA_SEP +
                     DatabaseColumns.DelayContacts.COLUMN_PHONE + TEXT_TYPE + COMA_SEP +
-                    DatabaseColumns.DelayContacts.COLUMN_TIME_LAST_DELAYED_CALL +
+                    DatabaseColumns.DelayContacts.COLUMN_TIME_LAST_DELAYED_CALL + DATETIME +
                     ")";
     private static final String DELETE_TABLE = "DROP TABLE IF EXISTS ";
     public static final String DROP_DELAY_CONTACTS =
