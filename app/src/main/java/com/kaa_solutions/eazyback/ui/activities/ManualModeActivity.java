1aa05ed3d7cc9e40172cf882757f4cdba4cb4593
Ponomarenko <albka1986@gmail.com>
2017-03-13 09:12:49 +0200

    create activity Button; added several fields on the screen;

---
+++ app/src/main/java/com/kaa_solutions/eazyback/ui/activities/ManualModeActivity.java
@@ -0,0 +1,14 @@
+package com.kaa_solutions.eazyback.ui.activities;
+
+import android.os.Bundle;
+
+import com.kaa_solutions.eazyback.R;
+
+public class ManualModeActivity extends GenericActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_mode);
+    }
+}
