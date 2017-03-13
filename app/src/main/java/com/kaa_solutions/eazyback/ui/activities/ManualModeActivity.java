41a45458fa10d972ffcccf63699170758a4d52a1
Ponomarenko <albka1986@gmail.com>
2017-03-14 00:56:37 +0200

    added all fields for the screen "Buttons" (margins are not done completely);

--- app/src/main/java/com/kaa_solutions/eazyback/ui/activities/ManualModeActivity.java
+++ app/src/main/java/com/kaa_solutions/eazyback/ui/activities/ManualModeActivity.java
@@ -1,14 +1,19 @@
 package com.kaa_solutions.eazyback.ui.activities;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import com.kaa_solutions.eazyback.R;
 
 public class ManualModeActivity extends GenericActivity {
 
+    private Switch mCallbackActivatedSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_mode);
+
+        initBackButton();
     }
 }
