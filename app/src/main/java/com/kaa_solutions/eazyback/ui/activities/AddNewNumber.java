e59c4312629be51c3bed050aa74fe6e31ae82f43
Ponomarenko <albka1986@gmail.com>
2017-03-21 20:20:59 +0200

    sketched a screen "Add a New Number";

---
+++ app/src/main/java/com/kaa_solutions/eazyback/ui/activities/AddNewNumber.java
@@ -0,0 +1,15 @@
+package com.kaa_solutions.eazyback.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kaa_solutions.eazyback.R;
+
+public class AddNewNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_number);
+    }
+}
